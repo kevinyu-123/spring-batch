560b644d93ca28dbb18f1012a56bbb5bccb119cb
hj <tntj123@naver.com>
2022-12-18 22:26:36 +0900

    fixed untracked files

--- src/main/java/com/study/batch/batch/job/DataMigrationConfig.java
+++ src/main/java/com/study/batch/batch/job/DataMigrationConfig.java
@@ -9,10 +9,20 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -36,16 +46,35 @@ public class DataMigrationConfig {
                 .incrementer(new RunIdIncrementer())
                 .start(migrationStep)
                 .build();
-
     }
 
     @Bean
     @JobScope
-    public Step migrationStep(){
+    public Step migrationStep(ItemReader trOrderReader){
         return stepBuilderFactory.get("migrationStep")
-                .<Order,Order>chunk(5)
+                //db에서 읽어오는데이터 타입, writer 할 데이터 타입
+                .<Order,Order>chunk(5) // chunk: 5개의 데이터 단위로 처리(transaction의 개수), 읽어오는 데이터 타입 : order, write 할 데이터 타입 : order
+                .reader(trOrderReader)
+                .writer(new ItemWriter() {
+                    @Override
+                    public void write(List items) throws Exception {
+                        items.forEach(System.out::println);
+                    }
+                })
                 .build();
     }
 
+    @Bean
+    @StepScope
+    public RepositoryItemReader<Order> trOrderReader(){
+        return new RepositoryItemReaderBuilder<Order>()
+                .name("trOrderReader")
+                .repository(orderRepository)
+                .methodName("findAll") //repository method
+                .pageSize(5) // chunk 사이즈와 동일
+                .arguments(Arrays.asList())
+                .sorts(Collections.singletonMap("id", Sort.Direction.ASC)) //정렬
+                .build();
+    }
 
 }
