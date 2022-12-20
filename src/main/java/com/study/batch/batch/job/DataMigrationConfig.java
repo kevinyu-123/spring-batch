a2a72dfa2e1c29f76e1e0174e44b664d73167b48
hj <tntj123@naver.com>
2022-12-20 23:32:10 +0900

    item writer added

--- src/main/java/com/study/batch/batch/job/DataMigrationConfig.java
+++ src/main/java/com/study/batch/batch/job/DataMigrationConfig.java
@@ -14,8 +14,11 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
+import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +26,7 @@ import org.springframework.data.domain.Sort;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
@@ -55,14 +59,8 @@ public class DataMigrationConfig {
                 //db에서 읽어오는데이터 타입, writer 할 데이터 타입
                 .<Order, Account>chunk(5) // chunk: 5개의 데이터 단위로 처리(transaction의 개수), 읽어오는 데이터 타입 : order, write 할 데이터 타입 : order
                 .reader(trOrderReader)
-//                .writer(new ItemWriter() {
-//                    @Override
-//                    public void write(List items) throws Exception {
-//                        items.forEach(System.out::println);
-//                    }
-//                })
                 .processor(orderProcessor())
-                .writer()
+                .writer(repOrderWriter())
                 .build();
     }
 
@@ -77,6 +75,29 @@ public class DataMigrationConfig {
         };
     }
 
+    @Bean
+    @StepScope
+    public RepositoryItemWriter<Account> repOrderWriter(){
+
+        return new RepositoryItemWriterBuilder<Account>()
+                .repository(accountRepository) //사용 레파지토리
+                .methodName("save") // 레파지토리에서 사용하는 메소드
+                .build();
+    }
+
+    /*
+      RepositoryItemWriter를 사용하지 않고 직접 save 구현
+    */
+    @Bean
+    @StepScope
+    public ItemWriter<Account> orderWriter(){
+        return new ItemWriter<Account>() {
+            @Override
+            public void write(List<? extends Account> items) throws Exception {
+                      items.forEach(item -> accountRepository.save(item));
+            }
+        };
+    }
 
 
     @Bean
